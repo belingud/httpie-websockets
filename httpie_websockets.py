7c72f050dfc88dadffa0c1e12d3468ddaacc51a3
belingud <1170202353@qq.com>
2024-07-23 10:44:39 +0800

    Bump version: 0.0.2 → 0.0.3

--- httpie_websockets.py
+++ httpie_websockets.py
@@ -14,7 +14,7 @@ from requests.adapters import BaseAdapter
 from requests.models import PreparedRequest, Response
 
 warnings.simplefilter("ignore", category=RuntimeWarning)
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 
 
 class WebsocketAdapter(BaseAdapter):
