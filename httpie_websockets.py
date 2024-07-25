47bd1e4d84e0cb3551066a348eb861958a8752a3
belingud <1170202353@qq.com>
2024-07-25 22:51:07 +0800

    Bump version: 0.2.0 → 0.2.1

--- httpie_websockets.py
+++ httpie_websockets.py
@@ -15,7 +15,7 @@ from requests.models import PreparedRequest, Response
 from requests.structures import CaseInsensitiveDict
 
 warnings.simplefilter("ignore", category=RuntimeWarning)
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 
 class AdapterError(Exception):
